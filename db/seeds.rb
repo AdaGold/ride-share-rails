73672ad66540943dc743e3cf2e70d230f5cbdc6b
Dan Roberts <dan@adadevelopersacademy.org>
2017-09-23 12:30:41 -0700

    Add PK reset to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,15 @@ CSV.foreach(TRIP_FILE, :headers => true) do |row|
 end
 
 puts "Added #{Trip.count} trip records"
+
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
