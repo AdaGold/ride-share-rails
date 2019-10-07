df545dcc95136be94fe420c12840d6e1fe02735d
dee del rosario <dee@adadevelopersacademy.org>
2019-10-07 11:11:42 -0700

    conforms test stub names to be response focused, and to include conventions taught in C12. adds hints to arrange/act/assert, comment about test names

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,58 +1,175 @@
 require "test_helper"
 
 describe DriversController do
+  # Note: If any of these tests have names that conflict with either the requirements or your team's decisions, feel empowered to change the test names. For example, if a given test name says "responds with 404" but your team's decision is to respond with redirect, please change the test name.
+
   describe "index" do
-    it "responds with list of Drivers when there are drivers in db" do
-      # Your code here
+    it "responds with success when there are many drivers saved" do
+      # Arrange
+      # Ensure that there is at least one Driver saved
+
+      # Act
+
+      # Assert
+
     end
 
-    it "Provides clear messaging to user when there are no drivers in db" do
-      # Your code here
+    it "responds with success when there are no drivers saved" do
+      # Arrange
+      # Ensure that there are zero drivers saved
+
+      # Act
+
+      # Assert
+
     end
   end
 
   describe "show" do
-    it "Shows all relavant driver information and aggregated data when Driver exists" do
+    it "responds with success when showing an existing valid driver" do
+      # Arrange
+      # Ensure that there is a driver saved
+
+      # Act
+
+      # Assert
+
     end
 
-    it "Responds with not_found when Driver does not exist" do
+    it "responds with 404 with an invalid driver id" do
+      # Arrange
+      # Ensure that there is an id that points to no driver
+
+      # Act
+
+      # Assert
+
     end
   end
 
   describe "edit" do
-    it "Provides all relevant driver fiels to be updated when driver exists" do
+    it "responds with success when getting the edit page for an existing, valid driver" do
+      # Arrange
+      # Ensure there is an existing driver saved
+
+      # Act
+
+      # Assert
+
     end
 
-    it "Responds with not_found when Driver does not exist" do
+    it "responds with redirect when getting the edit page for a non-existing driver" do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+
+      # Act
+
+      # Assert
+
     end
   end
 
   describe "update" do
-    it "Updates all relevant driver fields to be updated when driver exists" do
+    it "can update an existing driver with valid information accurately, and redirect" do
+      # Arrange
+      # Ensure there is an existing driver saved
+      # Assign the existing driver's id to a local variable
+      # Set up the form data
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+
+      # Assert
+      # Use the local variable of an existing driver's id to find the driver again, and check that its attributes are updated
+      # Check that the controller redirected the user
+
     end
 
-    it "Responds with not_found when Driver does not exist" do
+    it "does not update any driver if given an invalid id, and responds with a 404" do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+      # Set up the form data
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+
+      # Assert
+      # Check that the controller gave back a 404
+
+    end
+
+    it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
+      # Note: This will not pass until ActiveRecord Validations lesson
+      # Arrange
+      # Ensure there is an existing driver saved
+      # Assign the existing driver's id to a local variable
+      # Set up the form data so that it violates Driver validations
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+
+      # Assert
+      # Check that the controller redirects
+
     end
   end
 
   describe "new" do
-    it "Displays all relevant driver fiels to be given by user" do
+    it "responds with success" do
     end
   end
 
   describe "create" do
-    it "Creates new Driver in db with fields given by user" do
+    it "can create a new driver with valid information accurately, and redirect" do
+      # Arrange
+      # Set up the form data
+
+      # Act-Assert
+      # Ensure that there is a change of 1 in Driver.count
+
+      # Assert
+      # Find the newly created Driver, and check that all its attributes match what was given in the form data
+      # Check that the controller redirected the user
+
     end
 
-    it "Redirects user back to new_driver page when a required field is missing" do
+    it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
+      # Note: This will not pass until ActiveRecord Validations lesson
+      # Arrange
+      # Set up the form data so that it violates Driver validations
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+
+      # Assert
+      # Check that the controller redirects
+
     end
   end
 
   describe "destroy" do
-    it "Destroys the driver instance in db when driver exists" do
+    it "destroys the driver instance in db when driver exists, then redirects" do
+      # Arrange
+      # Ensure there is an existing driver saved
+
+      # Act-Assert
+      # Ensure that there is a change of -1 in Driver.count
+
+      # Assert
+      # Check that the controller redirects
+
     end
 
-    it "Responds with not_found when Driver does not exist" do
+    it "does not change the db when the driver does not exist, then responds with " do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+
+      # Assert
+      # Check that the controller responds or redirects with whatever your group decides
+
     end
   end
 end
