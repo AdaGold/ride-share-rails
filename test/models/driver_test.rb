0fc14b260849adf1d8d5683707934ac7b8dcee95
Chris M <chris@adadevelopersacademy.org>
2020-05-12 09:38:50 -0700

    Delete Hidden character on line 30

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -27,7 +27,7 @@ describe Driver do
       new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
       trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
       trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
-​
+
       # Assert
       expect(new_driver.trips.count).must_equal 2
       new_driver.trips.each do |trip|
